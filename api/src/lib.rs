481f99d8dd887cd6d049a721b4b7665658a47005
Thorsten Hans <thans@akamai.com>
2026-08-20 13:09:44 +0200

    chore: lower-case variable.. obviously
    Signed-off-by: Thorsten Hans <thans@akamai.com>

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -11,7 +11,7 @@ use crate::discord::DiscordNotifier;
 const MESSAGES: [&str; 3] = [
     "We'll be live soon and build something amazing with Spin 💫!",
     "On Air soon! Get ready for an exciting journey with Spin 🚀!",
-    "Huddle is starting in a few! Join us for some Spin magic ✨!"
+    "Huddle is starting in a few! Join us for some Spin magic ✨!",
 ];
 
 mod discord;
@@ -30,15 +30,17 @@ async fn send_notification() -> StatusCode {
         .as_nanos() as usize)
         % MESSAGES.len()];
     let mut notifier = DiscordNotifier::new(
-        variables::get("DISCORD_WEBHOOK_URL")
-            .await.expect("Invalid App Config")
+        variables::get("discord_webhook_url")
+            .await
+            .expect("Invalid App Config"),
     );
     match notifier
         .with_message(random_message.to_string())
-        .send().await {
+        .send()
+        .await
+    {
         Ok(_) => StatusCode::OK,
-        Err(e) => 
-        {
+        Err(e) => {
             println!("Error sending notification: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         }
